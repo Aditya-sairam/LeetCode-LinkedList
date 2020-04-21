aaa63b914a05c9054d84b107625b7da50de00747
Aditya-sairam <37501823+Aditya-sairam@users.noreply.github.com>
2020-04-21 18:30:38 +0530

    Removing the given value node from the linked list

---
+++ Remove.java
@@ -0,0 +1,26 @@
+//Removing the given value node from the linked list
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+       
+        
+        if(head == null){
+            return null;
+        }
+        while(head!=null && head.val == val){
+            head = head.next;
+        }
+         ListNode curr = head; 
+        ListNode prev = null;
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+        }
+    }
